9d53553e92612b7e7024eafa230d62478d03b15a
aoba <ipbdipbd00@gmail.com>
2019-03-23 11:08:16 +0900

    Exec bundle annotate

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer          not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Relationship < ApplicationRecord
   # follower_idがUserに紐づく
   belongs_to :follower, class_name: "User"
